7cd12d6423262ffe99e08840e11076c44b054e6d
IgorUstimenkoDesktop <igorustimeko@yandex.ru>
2020-08-26 13:36:54 +0300

    Страница с информацией

--- static/ligh_zoom/lightzoom.js
+++ static/ligh_zoom/lightzoom.js
@@ -1,27 +1,21 @@
-/**
- * jQuery lightzoom v1.0.1
- * Copyright 2018
- * Contributing Author: Aleksey Savin
- * E-mail: asavin.work@yandex.ru
- */
+;
+(function ($, window, document, undefined) {
 
- ;( function( $, window, document, undefined ) {
+  "use strict";
 
- 	"use strict";
+  // Create the defaults once
+  var pluginName = "lightzoom",
+    defaults = {
+      speed: 400,
+      imgPadding: 10,
+      overlayOpacity: '0.5',
+      viewTitle: false,
+      isOverlayClickClosing: false,
+      isWindowClickClosing: false,
+      isEscClosing: false
+    },
 
- 		// Create the defaults once
- 		var pluginName = "lightzoom",
- 			defaults = {
-        speed:                  400,
-        imgPadding:             10,
-        overlayOpacity:         '0.5',
-        viewTitle:              false,
-        isOverlayClickClosing:  false,
-        isWindowClickClosing:   false,
-        isEscClosing:           false
- 			},
-
-      html  = '<div id="lz-container">\
+    html = '<div id="lz-container">\
                  <div id="lz-box">\
                    <div id="lz-overlay"></div>\
                  </div>\
@@ -34,187 +28,197 @@
                  </div>\
                </div>';
 
- 		// The actual plugin constructor
- 		function Plugin ( element, options ) {
- 			this.element = element;
+  // The actual plugin constructor
+  function Plugin(element, options) {
+    this.element = element;
 
- 			this.settings = $.extend( {}, defaults, options );
- 			this._defaults = defaults;
- 			this._name = pluginName;
- 			this.init();
- 		}
+    this.settings = $.extend({}, defaults, options);
+    this._defaults = defaults;
+    this._name = pluginName;
+    this.init();
+  }
 
- 		// Avoid Plugin.prototype conflicts
- 		$.extend( Plugin.prototype, {
- 			init : function() {
+  // Avoid Plugin.prototype conflicts
+  $.extend(Plugin.prototype, {
+    init: function () {
 
-        var $this = this;
+      var $this = this;
 
-        $this.build();
+      $this.build();
 
-        $( document ).on( 'click', '#lz-close', function( event ) {
-          event.preventDefault();
+      $(document).on('click', '#lz-close', function (event) {
+        event.preventDefault();
+        $this.closeZoom();
+
+      });
+
+      if ($this.settings.isWindowClickClosing) {
+        $(document).on('click', '#lz-container', function (event) {
           $this.closeZoom();
         });
+      } else if ($this.settings.isOverlayClickClosing) {
+        $(document).on('click', '#lz-overlay', function (event) {
+          $this.closeZoom();
+        });
+      }
 
-        if ( $this.settings.isWindowClickClosing ) {
-          $( document ).on( 'click', '#lz-container', function( event ) {
+      if ($this.settings.isEscClosing) {
+        $(document).keydown(function (event) {
+          if (event.which === 27) {
             $this.closeZoom();
-          });
-        } else if ( $this.settings.isOverlayClickClosing ) {
-          $( document ).on( 'click', '#lz-overlay', function( event ) {
-            $this.closeZoom();
-          });
-        }
+          }
+        });
+      }
 
-        if ( $this.settings.isEscClosing ) {
-          $( document ).keydown( function( event ) {
-            if ( event.which === 27 ) {
-              $this.closeZoom();
-            }
-          });
-        }
+      $this.resize();
+    },
 
-        $this.resize();
- 			},
+    build: function () {
+      var $this = this;
 
-      build : function () {
-        var $this = this;
+      $(this.element).on('click', function (event) {
+        event.preventDefault();
+        console.log('Открылся');
+        // body.style.overflow = 'hidden';
+        $('body').append(html);
 
-        $(this.element).on( 'click', function( event ) {
-          event.preventDefault();
 
-          $( 'body' ).append( html );
+        var lz = $('#lz-box'),
+          img = $(this).children('img'),
+          src = $(this).attr('href'),
+          overlayCss = {};
 
-          var lz      = $( '#lz-box' ),
-            img       = $(this).children('img'),
-            src       = $(this).attr('href'),
-            overlayCss = {};
+        var title = img.attr('title');
 
-          var title = img.attr('title');
+        overlayCss = {
+          opacity: $this.settings.overlayOpacity
+        };
+        $('#lz-overlay').css(overlayCss);
+        $('#lz-container').fadeIn({
+          'display': 'block'
+        });
 
-          overlayCss = {
-            opacity : $this.settings.overlayOpacity
-          };
-          $('#lz-overlay').css( overlayCss );
-          $('#lz-container').fadeIn({ 'display' : 'block' });
+        $(new Image()).attr('src', src).on('load', function () {
 
-          $(new Image()).attr('src', src).on( 'load', function() {
+          lz.append('<img src="' + src + '">');
 
-            lz.append( '<img src="' + src + '">' );
+          $this.showTitle(title);
 
-            $this.showTitle( title );
+          $this.setPadding(title);
 
-            $this.setPadding( title );
+          $this.setDim();
 
-            $this.setDim();
+          lz.append('<a href="#" id="lz-close" title="Close">Close</a>');
 
-            lz.append( '<a href="#" id="lz-close" title="Close">Close</a>' );
+          $('#loading-center').remove();
 
-            $('#loading-center').remove();
-
-            $('#lz-box img').animate({opacity: 1}, $this.settings.speed);
-
-          });
+          $('#lz-box img').animate({
+            opacity: 1
+          }, $this.settings.speed);
 
         });
 
-      },
+      });
 
-      showTitle : function (title) {
-        if ( this.settings.viewTitle && title ) {
-          $( '#lz-box' ).append( '<p>' + title + '</p>' );
-        }
-      },
+    },
 
-      setPadding : function (title) {
-        var padding = 0,
-         backgroundColor = this.settings.imgPadding > 0 ? "#FFF" : "none",
-         imgCss = {};
+    showTitle: function (title) {
+      if (this.settings.viewTitle && title) {
+        $('#lz-box').append('<p>' + title + '</p>');
+      }
+    },
 
-        if ( this.settings.viewTitle && title ) {
-          padding = this.settings.imgPadding + "px " + this.settings.imgPadding + "px " + ( this.settings.imgPadding + 30 ) + "px " + this.settings.imgPadding + "px";
-        } else {
-          padding = this.settings.imgPadding;
-        }
+    setPadding: function (title) {
+      var padding = 0,
+        backgroundColor = this.settings.imgPadding > 0 ? "#FFF" : "none",
+        imgCss = {};
 
-        imgCss = {
-          padding         : padding,
-          backgroundColor : backgroundColor
-        }
+      if (this.settings.viewTitle && title) {
+        padding = this.settings.imgPadding + "px " + this.settings.imgPadding + "px " + (this.settings.imgPadding + 30) + "px " + this.settings.imgPadding + "px";
+      } else {
+        padding = this.settings.imgPadding;
+      }
 
-        $('#lz-box img').css( imgCss );
-      },
+      imgCss = {
+        padding: padding,
+        backgroundColor: backgroundColor
+      }
 
-			setDim : function () {
+      $('#lz-box img').css(imgCss);
+    },
 
-				var width, height, imgCss = {};
-        var winWidth = window.innerWidth ? window.innerWidth : $( window ).width();
-        var winHeight = window.innerHeight ? window.innerHeight : $( window ).height();
+    setDim: function () {
 
-        if ( winWidth > winHeight ) {
-          width  = "100%";
-          height = "80%";
-        } else {
-          width  = "80%";
-          height = "100%";
-        }
+      var width, height, imgCss = {};
+      var winWidth = window.innerWidth ? window.innerWidth : $(window).width();
+      var winHeight = window.innerHeight ? window.innerHeight : $(window).height();
 
-				// Reset dimensions on mobile orientation change
-				if ( 'onorientationchange' in window ) {
+      if (winWidth > winHeight) {
+        width = "100%";
+        height = "80%";
+      } else {
+        width = "80%";
+        height = "100%";
+      }
 
-					window.addEventListener( 'orientationchange', function() {
+      // Reset dimensions on mobile orientation change
+      if ('onorientationchange' in window) {
 
-            if (window.orientation === 0) {
-              width = "80%";
-              height = "100%";
-            } else if (window.orientation === 90 || window.orientation === -90) {
-              width = "100%";
-              height = "80%";
-            }
-					}, false );
+        window.addEventListener('orientationchange', function () {
 
-				}
+          if (window.orientation === 0) {
+            width = "80%";
+            height = "100%";
+          } else if (window.orientation === 90 || window.orientation === -90) {
+            width = "100%";
+            height = "80%";
+          }
+        }, false);
 
-				imgCss = {
-            maxWidth        : width,
-            maxHeight       : height,
-				};
+      }
 
-				$( '#lz-box img' ).css( imgCss );
+      imgCss = {
+        maxWidth: width,
+        maxHeight: height,
+      };
 
-			},
+      $('#lz-box img').css(imgCss);
 
-      closeZoom : function () {
-        var $this = this;
+    },
 
-        $('#lz-container').fadeOut($this.settings.speed, function() {
-          $this.destroy();
-        });
+    closeZoom: function () {
+      var $this = this;
 
-			},
+      $('#lz-container').fadeOut($this.settings.speed, function () {
+        $this.destroy();
+      });
 
-			resize : function () {
-				var $this = this;
 
-				$( window ).resize( function() {
-					$this.setDim();
-				} ).resize();
-			},
+    },
 
-      destroy : function () {
-        $('#lz-container').remove();
-      },
+    resize: function () {
+      var $this = this;
 
- 		});
+      $(window).resize(function () {
+        $this.setDim();
+      }).resize();
+    },
 
- 		$.fn[ pluginName ] = function( options ) {
- 			return this.each( function() {
- 				if ( !$.data( this, "plugin_" + pluginName ) ) {
- 					$.data( this, "plugin_" +
- 						pluginName, new Plugin( this, options ) );
- 				}
- 			} );
- 		};
+    destroy: function () {
+      $('#lz-container').remove();
+      // body.style.overflow = 'visible';
+      console.log('Закрылся');
+    },
 
- } )( jQuery, window, document );
+  });
+
+  $.fn[pluginName] = function (options) {
+    return this.each(function () {
+      if (!$.data(this, "plugin_" + pluginName)) {
+        $.data(this, "plugin_" +
+          pluginName, new Plugin(this, options));
+      }
+    });
+  };
+
+})(jQuery, window, document);
